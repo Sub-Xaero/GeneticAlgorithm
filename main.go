dcf43d1bd2a34b1b2e6256c2e7134878015db393
SubXaero <dylcla@me.com>
2017-10-02 19:17:03 +0100

    Define preliminary fitness function as reciever of Genome

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 type Genome struct {
-	score    int64
 	sequence string
+}
+
+func (g Genome) fitness() int64 {
+	var score int64
+	score, err := strconv.ParseInt(g.sequence, 2, 32)
+	if err == nil {
+		return score
+	} else {
+		panic(errors.New("could not parse bitstring"))
+	}
+}
+
+func generateBitString(length int) (string, error) {
+	if length <= 0 {
+		return "", errors.New("strings cannot be zero-length")
+	}
+	var bitstring string
+	for i := 0; i < length; i++ {
+		bitstring += strconv.Itoa(rand.Int() % 2)
+	}
+	return bitstring, nil
 }
 
 func main() {
