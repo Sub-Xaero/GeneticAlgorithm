26f8672d4341e238d63722d2b3d7e41788b1ccf9
SubXaero <dylcla@me.com>
2017-10-07 18:52:51 +0100

    Tests - Custom fitness function

--- fitness_test.go
+++ fitness_test.go
@@ -5,7 +5,25 @@ import (
 	"testing"
 )
 
-func TestGenome_Fitness(t *testing.T) {
+func TestGenome_DefaultFitness(t *testing.T) {
+	genome := Genome{"1111"}
+
+	if genome.Fitness() != 4 {
+		t.Error("String is not correct fitness")
+	}
+
+	genome = Genome{"0011"}
+
+	SetFitnessFunc(func(gene Genome) int {
+		return strings.Count(gene.Sequence, "0")
+	})
+
+	if genome.Fitness() != 2 {
+		t.Error("String is not correct fitness")
+	}
+}
+
+func TestGenome_CustomFitness(t *testing.T) {
 	genome := Genome{"1111"}
 
 	SetFitnessFunc(func(gene Genome) int {
