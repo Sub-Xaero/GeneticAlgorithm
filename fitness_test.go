56d19e26d721d6d0d5331488221431612573b089
SubXaero <dylcla@me.com>
2017-10-07 15:38:58 +0100

    Tests - Test fitness function

---
+++ fitness_test.go
@@ -0,0 +1,29 @@
+package ga
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGenome_Fitness(t *testing.T) {
+	genome := Genome{"1111"}
+
+	SetFitnessFunc(func(gene Genome) int {
+		return strings.Count(gene.Sequence, "1")
+	})
+
+	if genome.Fitness() != 4 {
+		t.Error("String is not correct fitness")
+	}
+
+	genome = Genome{"0011"}
+
+	SetFitnessFunc(func(gene Genome) int {
+		return strings.Count(gene.Sequence, "0")
+	})
+
+	if genome.Fitness() != 2 {
+		t.Error("String is not correct fitness")
+	}
+}
+
