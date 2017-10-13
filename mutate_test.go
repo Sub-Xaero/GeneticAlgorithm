3db920b72f2c00bf5d789655ebb687f45cc4a808
SubXaero <dylcla@me.com>
2017-10-14 00:00:03 +0100

    Fix mutate test

--- mutate_test.go
+++ mutate_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func TestSetMutateFunc(t *testing.T) {
+	SetFitnessFunc(DefaultFitnessFunc)
 	SetMutateFunc(func(gene Genome) Genome {
 		return Genome{[]int{1, 2, 3, 4}}
 	})
 
-	if fmt.Sprint(Genome{[]int{}}.Mutate()) != "{[1 2 3 4],   1}" {
-		t.Error("Mutate function not set")
+	output := fmt.Sprint(Genome{[]int{}}.Mutate())
+	if output != "{[1 2 3 4],   1}" {
+		t.Error("Mutate function not set", output)
 	}
 }
