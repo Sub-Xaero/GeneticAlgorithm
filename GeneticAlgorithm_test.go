835a3d999eabea7774d6ca0b8333da60661c463f
SubXaero <dylcla@me.com>
2017-10-13 21:45:00 +0100

    Test Fill Random Population

--- GeneticAlgorithm_test.go
+++ GeneticAlgorithm_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func TestFillRandomPopulation(t *testing.T) {
+	population := make([]Genome, 0)
+	population = FillRandomPopulation(population, 10, 10)
+
+	if len(population) != 10 {
+		t.Error("Population not filled to specified size")
+	}
+
+	for _, val := range population {
+		if len(val.Sequence) == 0 {
+			t.Error("Bitstrings in population are empty")
+		}
+	}
+}
 
 func TestTournament(t *testing.T) {
 	population := []Genome{
