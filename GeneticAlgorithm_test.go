601495e78fd9f580f25851c8584d9c7ff2b32966
SubXaero <dylcla@me.com>
2017-10-13 21:44:34 +0100

    Test crossover

---
+++ GeneticAlgorithm_test.go
@@ -0,0 +1,24 @@
+package ga
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCrossover(t *testing.T) {
+	population := []Genome{
+		{[]int{1, 0, 0, 0}},
+		{[]int{0, 0, 0, 1}},
+	}
+	offspring := population[0].Crossover(population[1])
+	found := false
+	for _, val := range offspring {
+		if fmt.Sprint(val) == "{[1 0 0 1],   2}" {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Error("Crossover failed")
+	}
+}
