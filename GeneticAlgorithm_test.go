1ea8a5ef2ab38fa037a4f4172adb677ec80215a1
SubXaero <dylcla@me.com>
2017-10-13 23:24:23 +0100

    Test SetMutate

--- GeneticAlgorithm_test.go
+++ GeneticAlgorithm_test.go
@@ -64,3 +64,13 @@ func TestSetCrossoverFunc(t *testing.T) {
 	}
 }
 
+
+func TestSetMutateFunc(t *testing.T) {
+	SetMutateFunc(func(gene Genome) Genome {
+		return Genome{[]int{1, 2, 3, 4}}
+	})
+
+	if fmt.Sprint(Genome{[]int{}}.Mutate()) != "{[1 2 3 4],   1}" {
+		t.Error("Mutate function not set")
+	}
+}
