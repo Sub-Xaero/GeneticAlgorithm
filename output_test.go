e222bc06e366db23706c98c067fc0ce374fdee0e
Dylan Clarke <dylcla@me.com>
2017-10-20 23:12:08 +0100

    Test - SetOutput

---
+++ output_test.go
@@ -0,0 +1,27 @@
+package ga
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestSetOutputFunc(t *testing.T) {
+	var genA GeneticAlgorithm
+	var gotOutput string
+	output := func (s string) {
+		gotOutput = s
+	}
+
+	genA.SetOutputFunc(func(a ...interface{}) {
+		output(fmt.Sprint(a))
+	})
+
+	genA.Output("output string")
+
+	expectedOutput := "[output string]"
+	if expectedOutput != gotOutput {
+		t.Error("Output func not set. Expected:", expectedOutput, "Got:", gotOutput)
+	} else {
+		t.Log("Output func set correctly. Expected:", expectedOutput, "Got:", gotOutput)
+	}
+}
