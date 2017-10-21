16869bd87372c68c47d4977813a31e1bdf221cf1
Dylan Clarke <dylcla@me.com>
2017-10-21 19:09:21 +0100

    Rules - Match "#", return error

--- rule.go
+++ rule.go
@@ -1,27 +1,30 @@
 package ga
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 type Rule struct {
 	condition bitstring
 	output    string
 }
 
-func (rule1 Rule) Matches(rule2 Rule) bool {
+func (rule1 Rule) Matches(rule2 Rule) (bool, error) {
 	if len(rule1.condition) != len(rule2.condition) {
-		return false
+		return false, errors.New("conditions are not same length")
 	}
 	conditionMatches := true
 	for i := range rule1.condition {
-		if rule1.condition[i] == "2" || rule2.condition[i] == "2" {
+		if string(rule1.condition[i]) == "#" || string(rule2.condition[i]) == "#" {
 			continue
-		} else if rule1.condition[i] != rule2.condition[i] {
+		} else if string(rule1.condition[i]) != string(rule2.condition[i]) {
 			conditionMatches = false
 			break
 		}
 	}
 	outputMatches := rule1.output == rule2.output
-	return outputMatches && conditionMatches
+	return outputMatches && conditionMatches, nil
 }
 
 func (r Rule) String() string {
