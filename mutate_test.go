dc531718db0556839a20031a3214bbc49f5b14ad
Dylan Clarke <dylcla@me.com>
2017-10-21 11:46:41 +0100

    Test - Default mutate func

--- mutate_test.go
+++ mutate_test.go
@@ -24,3 +24,16 @@ func TestSetMutateFunc(t *testing.T) {
 		t.Log("Mutate function was set successfully. Expected:", expectedOutput, "Got:", output)
 	}
 }
+
+func TestDefaultMutateFunc(t *testing.T) {
+	t.Parallel()
+	genA := NewGeneticAlgorithm()
+	gene := Genome{bitstring{"1", "0", "1", "0", "1"}}
+	geneOutput := genA.Mutate(gene)
+
+	if gene.String() == geneOutput.String() {
+		t.Error("Mutate did not change bitstrings. At least one mutation should occur. Was:", gene, "Mutated:", geneOutput)
+	} else {
+		t.Log("Mutate successfully changed bitstrings. At least one mutation should occur. Was:", gene, "Mutated:", geneOutput)
+	}
+}
